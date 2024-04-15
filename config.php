e2b146e50f8899b016154c4a2b40193f19f1ce4a
MandipRaut <119114781+MandipRaut@users.noreply.github.com>
2024-04-15 12:00:30 +0545

    Update config.php
    Added comments.

--- config.php
+++ config.php
@@ -1,16 +1,19 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','carrentalsystem');
+define('DB_HOST','localhost'); // Define the database host.
+define('DB_USER','root'); // Define the database username.
+define('DB_PASS',''); // Define the database password.
+define('DB_NAME','carrentalsystem'); // Define the name of the database.
+
 // Establish database connection.
 try
 {
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+    // Create a new PDO (PHP Data Objects) instance to connect to the database.
+    $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
 }
 catch (PDOException $e)
 {
-exit("Error: " . $e->getMessage());
+    // If an exception (error) occurs during the connection attempt, print an error message and exit the script.
+    exit("Error: " . $e->getMessage());
 }
 ?>
