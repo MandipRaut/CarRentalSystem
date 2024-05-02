99b9f5cfa5951f5aafff94300ac7c1f1c485320a
AnuragSharma8 <119913423+AnuragSharma8@users.noreply.github.com>
2024-05-02 12:52:21 +0545

    Update leftbar.php

--- Admin/includes/leftbar.php
+++ Admin/includes/leftbar.php
@@ -1,25 +1,17 @@
 <nav class="ts-sidebar">
-	<ul class="ts-sidebar-menu">
-		
-		<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-		<li><a href="#"><i class="fa fa-files-o"></i> Brands</a>
-		<ul>
-			<li><a href="#">Create Brand</a></li>
-			<li><a href="#">Manage Brands</a></li>
-		</ul>
-		</li>
-		<li><a href="#"><i class="fa fa-sitemap"></i> Vehicles</a>
-			<ul>
-				<li><a href="#">Post a Vehicle</a></li>
-				<li><a href="#">Manage Vehicles</a></li>
-			</ul>
-		</li>
-			<li><a href="#"><i class="fa fa-users"></i> Manage Booking</a></li>
-			<li><a href="#"><i class="fa fa-table"></i> Manage Testimonials</a></li>
-			<li><a href="#"><i class="fa fa-desktop"></i> Manage Conatctus Query</a></li>
-			<li><a href="#"><i class="fa fa-users"></i> Reg Users</a></li>
-			<li><a href="#"><i class="fa fa-files-o"></i> Manage Pages</a></li>
-			<li><a href="#"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
-			<li><a href="#"><i class="fa fa-table"></i> Manage Subscribers</a></li>
-	</ul>
+    <ul class="ts-sidebar-menu">
+        
+        <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="createbrand.php"><i class="fa fa-files-o"></i> Brand | Create Brand</a></li>
+        <li><a href="managebrand.php"><i class="fa fa-files-o"></i> Brand | Manage Brands</a></li>
+        <li><a href="postVehicle.php"><i class="fa fa-sitemap"></i> Vehicles | Post a Vehicle</a></li>
+        <li><a href="manageVehicle.php"><i class="fa fa-sitemap"></i> Vehicles | Manage Vehicles</a></li>
+        <li><a href="managebooking.php"><i class="fa fa-users"></i> Manage Booking</a></li>
+        <li><a href="manageReviews.php"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+        <li><a href="updatecontacts.php"><i class="fa fa-desktop"></i> Manage Contact Us</a></li>
+        <li><a href="users.php"><i class="fa fa-users"></i> Reg Users</a></li>
+        <li><a href="managePage.php"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+        <li><a href="updatecontacts.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+        <li><a href="subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+    </ul>
 </nav>
