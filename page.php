2fe52f011dcad6c7b3ad61f2de79bac9e4d04b0a
AnuragSharma8 <119913423+AnuragSharma8@users.noreply.github.com>
2024-05-04 19:50:40 +0545

    Update page.php

--- page.php
+++ page.php
@@ -1,9 +1,3 @@
-<?php
-session_start();
-error_reporting(0);
-include('config.php');
-?>
-
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -48,25 +42,14 @@ include('config.php');
         
 <!--Header-->
 <?php include('header.php');?>
-                      <?php 
-$pagetype=$_GET['type'];
-$sql = "SELECT type,detail,PageName from pages where type=:pagetype";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':pagetype',$pagetype,PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{ ?>
+                      
 <section class="page-header aboutus_page">
   <div class="container">
     <div class="page-header_wrap">
       <div class="page-heading">
-        <h1><?php   echo htmlentities($result->PageName); ?></h1>
+        <h1></h1>
         <!-- added -->
-        <a href="#">Home</a> -> <?php   echo htmlentities($result->PageName); ?>
+        <a href="#">Home</a> -> 
       </div>
     </div>
   </div>
@@ -78,10 +61,10 @@ foreach($results as $result)
     <div class="section-header text-center">
 
 
-      <h2><?php   echo htmlentities($result->PageName); ?></h2>
-      <p><?php  echo $result->detail; ?> </p>
+      <h2></h2>
+      <p> </p>
     </div>
-   <?php } }?>
+   
   </div>
 </section>
 <!-- /About-us--> 
