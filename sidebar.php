1ae5e170570402ad0b320b72440acc828f2117e6
AnuragSharma8 <119913423+AnuragSharma8@users.noreply.github.com>
2024-05-04 19:38:41 +0545

    Added frontend files

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+        <ul>
+            <li><a href="profile.php">Profile Settings</a></li>
+            <li><a href="updatePass.php">Update Password</a></li>
+            <li><a href="myBooking.php">My Booking</a></li>
+            <li><a href="postReviews.php">Post a Testimonial</a></li>
+            <li><a href="myReviews.php">My Reviews</a></li>
+            <li><a href="signout.php">Sign Out</a></li>
+        </ul>
+    </div>
+</div>
