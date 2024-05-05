e8bf6a3d69770af70e3346c2ac1dc81c30fa6966
MandipRaut <119114781+MandipRaut@users.noreply.github.com>
2024-05-05 12:21:00 +0545

    Update vehDetails.php
    added backend for vehicle details. User can now view vehicle details.

--- vehDetails.php
+++ vehDetails.php
@@ -1,3 +1,34 @@
+<?php
+session_start();
+include('config.php');
+error_reporting(0);
+if (isset($_POST['submit'])) {
+  $fromdate = $_POST['fromdate'];
+  $todate = $_POST['todate'];
+  $message = $_POST['message'];
+  $useremail = $_SESSION['login'];
+  $status = 0;
+  $vhid = $_GET['vhid'];
+  $sql = "INSERT INTO booking(userEmail,VehicleId,FromDate,ToDate,message,Status) VALUES(:useremail,:vhid,:fromdate,:todate,:message,:status)";
+  $query = $dbh->prepare($sql);
+  $query->bindParam(':useremail', $useremail, PDO::PARAM_STR);
+  $query->bindParam(':vhid', $vhid, PDO::PARAM_STR);
+  $query->bindParam(':fromdate', $fromdate, PDO::PARAM_STR);
+  $query->bindParam(':todate', $todate, PDO::PARAM_STR);
+  $query->bindParam(':message', $message, PDO::PARAM_STR);
+  $query->bindParam(':status', $status, PDO::PARAM_STR);
+  $query->execute();
+  $lastInsertId = $dbh->lastInsertId();
+  if ($lastInsertId) {
+    echo "<script>alert('Booking successfull.');</script>";
+  } else {
+    echo "<script>alert('Something went wrong. Please try again');</script>";
+  }
+}
+
+?>
+
+
 <!DOCTYPE HTML>
 <html lang="en">
 
@@ -44,17 +75,28 @@
   <?php include('header.php'); ?>
   <!-- /Header -->
 
-  
+  <?php
+  $vhid = intval($_GET['vhid']);
+  $sql = "SELECT vehicles.*,brands.BrandName,brands.id as bid  from vehicles join brands on brands.id=vehicles.VehiclesBrand where vehicles.id=:vhid";
+  $query = $dbh->prepare($sql);
+  $query->bindParam(':vhid', $vhid, PDO::PARAM_STR);
+  $query->execute();
+  $results = $query->fetchAll(PDO::FETCH_OBJ);
+  $cnt = 1;
+  if ($query->rowCount() > 0) {
+    foreach ($results as $result) {
+      $_SESSION['brndid'] = $result->bid;
+  ?>
 
       <section id="listing_img_slider">
-        <div><img src="admin/img/vehicleimages/" class="img-responsive" alt="image" width="900" height="560"></div>
-        <div><img src="admin/img/vehicleimages/" class="img-responsive" alt="image" width="900" height="560"></div>
-        <div><img src="admin/img/vehicleimages/" class="img-responsive" alt="image" width="900" height="560"></div>
-        <div><img src="admin/img/vehicleimages/" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="assets/images/<?php echo htmlentities($result->Vimage1); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="assets/images/<?php echo htmlentities($result->Vimage2); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="assets/images/<?php echo htmlentities($result->Vimage3); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
+        <div><img src="assets/images/<?php echo htmlentities($result->Vimage4); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
         <?php if ($result->Vimage5 == "") {
         } else {
         ?>
-          <div><img src="admin/img/vehicleimages/" class="img-responsive" alt="image" width="900" height="560"></div>
+          <div><img src="assets/images/<?php echo htmlentities($result->Vimage5); ?>" class="img-responsive" alt="image" width="900" height="560"></div>
         <?php } ?>
       </section>
       <!--/Listing-Image-Slider-->
@@ -65,11 +107,11 @@
         <div class="container">
           <div class="listing_detail_head row">
             <div class="col-md-9">
-              <h2></h2>
+              <h2><?php echo htmlentities($result->BrandName); ?> , <?php echo htmlentities($result->VehiclesTitle); ?></h2>
             </div>
             <div class="col-md-3">
               <div class="price_info">
-                <p> </p>Per Day
+                <p>$<?php echo htmlentities($result->PricePerDay); ?> </p>Per Day
 
               </div>
             </div>
@@ -80,16 +122,16 @@
                 <ul>
 
                   <li> <i class="fa fa-calendar" aria-hidden="true"></i>
-                    <h5></h5>
+                    <h5><?php echo htmlentities($result->ModelYear); ?></h5>
                     <p>Reg.Year</p>
                   </li>
                   <li> <i class="fa fa-cogs" aria-hidden="true"></i>
-                    <h5></h5>
+                    <h5><?php echo htmlentities($result->FuelType); ?></h5>
                     <p>Fuel Type</p>
                   </li>
 
                   <li> <i class="fa fa-user-plus" aria-hidden="true"></i>
-                    <h5></h5>
+                    <h5><?php echo htmlentities($result->SeatingCapacity); ?></h5>
                     <p>Seats</p>
                   </li>
                 </ul>
@@ -108,7 +150,7 @@
                     <!-- vehicle-overview -->
                     <div role="tabpanel" class="tab-pane active" id="vehicle-overview">
 
-                      <p></p>
+                      <p><?php echo htmlentities($result->VehiclesOverview); ?></p>
                     </div>
 
 
@@ -124,23 +166,32 @@
                         <tbody>
                           <tr>
                             <td>Air Conditioner</td>
-                           
+                            <?php if ($result->AirConditioner == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>AntiLock Braking System</td>
-                            
+                            <?php if ($result->AntiLockBrakingSystem == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Power Steering</td>
-                            
+                            <?php if ($result->PowerSteering == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
 
@@ -148,64 +199,91 @@
 
                             <td>Power Windows</td>
 
-                            
+                            <?php if ($result->PowerWindows == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>CD Player</td>
-                            
+                            <?php if ($result->CDPlayer == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Leather Seats</td>
-                            
+                            <?php if ($result->LeatherSeats == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Central Locking</td>
-                            
+                            <?php if ($result->CentralLocking == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Power Door Locks</td>
-                            
+                            <?php if ($result->PowerDoorLocks == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
                           <tr>
                             <td>Brake Assist</td>
-                            
+                            <?php if ($result->BrakeAssist == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php  } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Driver Airbag</td>
-                            
+                            <?php if ($result->DriverAirbag == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Passenger Airbag</td>
-                            
+                            <?php if ($result->PassengerAirbag == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                            
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                           <tr>
                             <td>Crash Sensor</td>
-                            
+                            <?php if ($result->CrashSensor == 1) {
+                            ?>
                               <td><i class="fa fa-check" aria-hidden="true"></i></td>
-                           
+                            <?php } else { ?>
+                              <td><i class="fa fa-close" aria-hidden="true"></i></td>
+                            <?php } ?>
                           </tr>
 
                         </tbody>
@@ -240,11 +318,11 @@
                   <div class="form-group">
                     <textarea rows="4" class="form-control" name="message" placeholder="Message" required></textarea>
                   </div>
-                  
+                  <?php if ($_SESSION['login']) { ?>
                     <div class="form-group">
                       <input type="submit" class="btn" name="submit" value="Book Now">
                     </div>
-                  
+                  <?php } else { ?>
                     <a href="#loginform" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login For Book</a>
 
                   <?php } ?>
@@ -261,20 +339,29 @@
           <div class="similar_cars">
             <h3>Similar Cars</h3>
             <div class="row">
-              <
+              <?php
+              $bid = $_SESSION['brndid'];
+              $sql = "SELECT vehicles.VehiclesTitle,brands.BrandName,vehicles.PricePerDay,vehicles.FuelType,vehicles.ModelYear,vehicles.id,vehicles.SeatingCapacity,vehicles.VehiclesOverview,vehicles.Vimage1 from vehicles join brands on brands.id=vehicles.VehiclesBrand where vehicles.VehiclesBrand=:bid";
+              $query = $dbh->prepare($sql);
+              $query->bindParam(':bid', $bid, PDO::PARAM_STR);
+              $query->execute();
+              $results = $query->fetchAll(PDO::FETCH_OBJ);
+              $cnt = 1;
+              if ($query->rowCount() > 0) {
+                foreach ($results as $result) { ?>
                   <div class="col-md-3 grid_listing">
                     <div class="product-listing-m gray-bg">
-                      <div class="product-listing-img"> <a href="vehical-details.php?vhid="><img src="admin/img/vehicleimages/>" class="img-responsive" alt="image" /> </a>
+                      <div class="product-listing-img"> <a href="vehDetails.php?vhid=<?php echo htmlentities($result->id); ?>"><img src="assets/images/<?php echo htmlentities($result->Vimage1); ?>" class="img-responsive" alt="image" /> </a>
                       </div>
                       <div class="product-listing-content">
-                        <h5><a href="vehDetails.php?vhid="> , </a></h5>
-                        <p class="list-price">$</p>
+                        <h5><a href="vehDetails.php?vhid=<?php echo htmlentities($result->id); ?>"><?php echo htmlentities($result->BrandName); ?> , <?php echo htmlentities($result->VehiclesTitle); ?></a></h5>
+                        <p class="list-price">$<?php echo htmlentities($result->PricePerDay); ?></p>
 
                         <ul class="features_list">
 
-                          <li><i class="fa fa-user" aria-hidden="true"></i> seats</li>
-                          <li><i class="fa fa-calendar" aria-hidden="true"></i> model</li>
-                          <li><i class="fa fa-car" aria-hidden="true"></i></li>
+                          <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity); ?> seats</li>
+                          <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear); ?> model</li>
+                          <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType); ?></li>
                         </ul>
                       </div>
                     </div>
