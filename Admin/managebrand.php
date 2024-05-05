81109b219c4ef1ece84050bfed7ac6061f69e169
Ridima Tuladhar <119678790+ridimatuladhar@users.noreply.github.com>
2024-05-05 21:14:44 +0545

    Update managebrand.php

--- Admin/managebrand.php
+++ Admin/managebrand.php
@@ -1,65 +1,58 @@
 <?php
-// Start the session
 session_start();
-// Suppress error reporting
 error_reporting(0);
-// Include the configuration file
 include('includes/config.php');
-// Check if the user is logged in, if not, redirect to the login page
-if(strlen($_SESSION['alogin'])==0){   
-    header('location:index.php');
+if(strlen($_SESSION['alogin'])==0)
+	{	
+header('location:index.php');
 }
 else{
-    // Check if the 'del' parameter is set in the URL
-    if(isset($_GET['del']))    {
-        // Get the ID of the record to be deleted
-        $id=$_GET['del'];
-        // SQL query to delete the record with the specified ID
-        $sql = "delete from brands  WHERE id=:id";
-        // Prepare the SQL query
-        $query = $dbh->prepare($sql);
-        // Bind the ID parameter
-        $query -> bindParam(':id',$id, PDO::PARAM_STR);
-        // Execute the query
-        $query -> execute();
-        // Set a success message
-        $msg="Page data updated successfully";
-    }
-}
-?>
+if(isset($_GET['del']))
+{
+$id=$_GET['del'];
+$sql = "delete from brands  WHERE id=:id";
+$query = $dbh->prepare($sql);
+$query -> bindParam(':id',$id, PDO::PARAM_STR);
+$query -> execute();
+$msg="Page data updated successfully";
 
+}
+
+
+
+ ?>
 
 <!doctype html>
 <html lang="en" class="no-js">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <meta name="theme-color" content="#3e454c">
-    
-    <title>Car Rental Portal |Admin Manage Brands   </title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="theme-color" content="#3e454c">
+	
+	<title>Admin Manage Brands</title>
 
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <!-- Sandstone Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- Bootstrap Datatables -->
-    <link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-    <!-- Bootstrap social button library -->
-    <link rel="stylesheet" href="css/bootstrap-social.css">
-    <!-- Bootstrap select -->
-    <link rel="stylesheet" href="css/bootstrap-select.css">
-    <!-- Bootstrap file input -->
-    <link rel="stylesheet" href="css/fileinput.min.css">
-    <!-- Awesome Bootstrap checkbox -->
-    <link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
-    <!-- Admin Stye -->
-    <link rel="stylesheet" href="css/style.css">
+	<!-- Font awesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Sandstone Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Bootstrap Datatables -->
+	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
+	<!-- Bootstrap social button library -->
+	<link rel="stylesheet" href="css/bootstrap-social.css">
+	<!-- Bootstrap select -->
+	<link rel="stylesheet" href="css/bootstrap-select.css">
+	<!-- Bootstrap file input -->
+	<link rel="stylesheet" href="css/fileinput.min.css">
+	<!-- Awesome Bootstrap checkbox -->
+	<link rel="stylesheet" href="css/awesome-bootstrap-checkbox.css">
+	<!-- Admin Stye -->
+	<link rel="stylesheet" href="css/style.css">
   <style>
-        .errorWrap {
+		.errorWrap {
     padding: 10px;
     margin: 0 0 20px 0;
     background: #fff;
@@ -75,54 +68,54 @@ else{
     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
 }
-        </style>
+		</style>
 
 </head>
 
 <body>
-    <?php include('includes/header.php');?>
+	<?php include('includes/header.php');?>
 
-    <div class="ts-main-content">
-        <?php include('includes/leftbar.php');?>
-        <div class="content-wrapper">
-            <div class="container-fluid">
+	<div class="ts-main-content">
+		<?php include('includes/leftbar.php');?>
+		<div class="content-wrapper">
+			<div class="container-fluid">
 
-                <div class="row">
-                    <div class="col-md-12">
+				<div class="row">
+					<div class="col-md-12">
 
-                        <h2 class="page-title">Manage Brands</h2>
+						<h2 class="page-title">Manage Brands</h2>
 
-                        <!-- Zero Configuration Table -->
-                        <div class="panel panel-default">
-                            <div class="panel-heading">Listed  Brands</div>
-                            <div class="panel-body">
-                            <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-                else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
-                                <table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
-                                    <thead>
-                                        <tr>
-                                        <th>#</th>
-                                                <th>Brand Name</th>
-                                            <th>Creation Date</th>
-                                            <th>Updation date</th>
-                                        
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tfoot>
-                                        <tr>
-                                        <th>#</th>
-                                            <th>Brand Name</th>
-                                            <th>Creation Date</th>
-                                            <th>Updation date</th>
-                                        
-                                            <th>Action</th>
-                                        </tr>
-                                        </tr>
-                                    </tfoot>
-                                    <tbody>
+						<!-- Zero Configuration Table -->
+						<div class="panel panel-default">
+							<div class="panel-heading">Listed  Brands</div>
+							<div class="panel-body">
+							<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+								<table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+									<thead>
+										<tr>
+										<th>#</th>
+												<th>Brand Name</th>
+											<th>Creation Date</th>
+											<th>Updation date</th>
+										
+											<th>Action</th>
+										</tr>
+									</thead>
+									<tfoot>
+										<tr>
+										<th>#</th>
+											<th>Brand Name</th>
+											<th>Creation Date</th>
+											<th>Updation date</th>
+										
+											<th>Action</th>
+										</tr>
+										</tr>
+									</tfoot>
+									<tbody>
 
-                                    <?php $sql = "SELECT * from brands ";
+									<?php $sql = "SELECT * from brands ";
 $query = $dbh -> prepare($sql);
 $query->execute();
 $results=$query->fetchAll(PDO::FETCH_OBJ);
@@ -130,44 +123,44 @@ $cnt=1;
 if($query->rowCount() > 0)
 {
 foreach($results as $result)
-{               ?>  
-                                        <tr>
-                                            <td><?php echo htmlentities($cnt);?></td>
-                                            <td><?php echo htmlentities($result->BrandName);?></td>
-                                            <td><?php echo htmlentities($result->CreationDate);?></td>
-                                            <td><?php echo htmlentities($result->UpdationDate);?></td>
+{				?>	
+										<tr>
+											<td><?php echo htmlentities($cnt);?></td>
+											<td><?php echo htmlentities($result->BrandName);?></td>
+											<td><?php echo htmlentities($result->CreationDate);?></td>
+											<td><?php echo htmlentities($result->UpdationDate);?></td>
 <td><a href="editBrand.php?id=<?php echo $result->id;?>"><i class="fa fa-edit"></i></a>&nbsp;&nbsp;
 <a href="managebrand.php?del=<?php echo $result->id;?>" onclick="return confirm('Do you want to delete');"><i class="fa fa-close"></i></a></td>
-                                        </tr>
-                                        <?php $cnt=$cnt+1; }} ?>
-                                        
-                                    </tbody>
-                                </table>
+										</tr>
+										<?php $cnt=$cnt+1; }} ?>
+										
+									</tbody>
+								</table>
 
-                        
+						
 
-                            </div>
-                        </div>
+							</div>
+						</div>
 
-                    
+					
 
-                    </div>
-                </div>
+					</div>
+				</div>
 
-            </div>
-        </div>
-    </div>
+			</div>
+		</div>
+	</div>
 
-    <!-- Loading Scripts -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap-select.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.dataTables.min.js"></script>
-    <script src="js/dataTables.bootstrap.min.js"></script>
-    <script src="js/Chart.min.js"></script>
-    <script src="js/fileinput.js"></script>
-    <script src="js/chartData.js"></script>
-    <script src="js/main.js"></script>
+	<!-- Loading Scripts -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap-select.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.dataTables.min.js"></script>
+	<script src="js/dataTables.bootstrap.min.js"></script>
+	<script src="js/Chart.min.js"></script>
+	<script src="js/fileinput.js"></script>
+	<script src="js/chartData.js"></script>
+	<script src="js/main.js"></script>
 </body>
 </html>
 <?php } ?>
