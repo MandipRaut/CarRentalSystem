d0bc624905a6358dc4a17813e8f39000ed16dca6
MandipRaut <119114781+MandipRaut@users.noreply.github.com>
2024-05-17 15:40:57 +0545

    Update sidebar.php
    fixed text

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
             <li><a href="profile.php">Profile Settings</a></li>
             <li><a href="updatePass.php">Update Password</a></li>
             <li><a href="myBooking.php">My Booking</a></li>
-            <li><a href="postReviews.php">Post a Testimonial</a></li>
+            <li><a href="postReviews.php">Post a Review</a></li>
             <li><a href="myReviews.php">My Reviews</a></li>
             <li><a href="signout.php">Sign Out</a></li>
         </ul>
